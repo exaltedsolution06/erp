2e8a884f56fa677799bb0b2118373bd2cf9df14a
Exalted 06 <exaltedsol06@gmail.com>
2025-11-11 11:45:17 +0530

    add

---
+++ a.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Hello, world!</title>
+    <style>
+p {
+  text-indent: 50px;
+}
+span {
+display: inline-block;
+width: 25%;
+border-bottom: dotted;
+}
+.span {
+display: inline-block;
+width: 70%;
+border-bottom: dotted;
+}
+</style>
+  </head>
+  <body>
+
+
+
+
+
+
+
+    <div class="container-fluid ">
+        <div class="row  p-3" style="border-left:5px solid red;border-right:5px solid red;border-top:1px solid ;border-bottom:1px solid">
+            <div class="col-1">
+                <img src="https://www.citypng.com/public/uploads/small/11639594360nclmllzpmer2dvmrgsojcin90qmnuloytwrcohikyurvuyfzvhxeeaveigoiajks5w2nytyfpix678beyh4ykhgvmhkv3r3yj5hi.png"  height="80"  alt="">
+            </div>
+            <div class="col-11 text-center">
+                <h2>GEETA NIKETAN SR. SEC. SCHOOL</h2>
+                <p>ARAINPERA ROAD, CHAURA DISTT. KARNAL</p>
+            </div>
+            <div class="col-12 mb-5 mt-3 text-center">
+                <h4>CHARACTER CERTIFICATE</h4>
+            </div>
+
+
+
+            <div class="col-12 text-left">
+
+                <p>This is to Cortify that <span>65</span>Father's Name<span>65</span>
+				Mother's Name<span>65</span>is/was bonafied student of this school in class  <span>65</span>during the year ,<span>65</span> He/She passed the <span>65</span>Examination of H.B.S.E. held in March<span>65</span>He/She secured'<span>65</span>Marks  and was placed in <span>65</span>Division. His/Her charecter and conduct are<span>65</span>His /Her Date of Birth acording to the school record is<span class="span">65</span></p>      
+
+
+            </div>
+
+            <div class="col-sm-12 mb-5 mt-5">
+              <div class="row">
+                <div class="col-6">
+                  Teacher-in-charge
+                </div>
+
+                <div class="col-6 " style="text-align: right;">
+                  Principal/HeadMaster
+                </div>
+              </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
