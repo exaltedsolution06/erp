279ec9b9597a3db8283eafecc7a909a26c952dc0
exaltedsol04 <exaltedsol04@gmail.com>
2025-12-10 11:11:26 +0530

    comment

--- application/controllers/Studentfee.php
+++ application/controllers/Studentfee.php
@@ -2021,7 +2021,6 @@ class Studentfee extends Admin_Controller
 
 
     public function studentfeelist(){
-        
         // if($_GET['type']=='delete'){
         //     $this->load->model('Receipt_model');
         //     if ($this->Receipt_model->delete_receipt($_GET['id'])) {
@@ -2126,13 +2125,13 @@ class Studentfee extends Admin_Controller
 
         $from_date = $this->input->get('from_date');
         $to_date   = $this->input->get('to_date');
-
+		$mode   = $this->input->get('mode');
 
         // die;
 
         // paginate
         $per_page_input = $this->input->get('per_page');
-        $total_rows = $this->Receipt_model->get_receipt_count($from_date, $to_date);
+        $total_rows = $this->Receipt_model->get_receipt_count($from_date, $to_date, $mode);
 
         $per_page = (!empty($per_page_input) && $per_page_input != 'all') ? (int)$per_page_input : 10;
         $per_page = ($per_page_input == 'all') ? $total_rows : $per_page;
@@ -2167,7 +2166,7 @@ class Studentfee extends Admin_Controller
         $this->pagination->initialize($config);
         $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
 
-        $data['receipt_data'] = $this->Receipt_model->get_receipt($config['per_page'], $page,$from_date, $to_date);
+        $data['receipt_data'] = $this->Receipt_model->get_receipt($config['per_page'], $page,$from_date, $to_date, $mode);
         $data['pagination_links'] = $this->pagination->create_links();
 
         // end paginate
