21e5c36b9dae5eabba8ebc78eb12ca94b26a4471
exaltedsol04 <exaltedsol04@gmail.com>
2025-12-12 17:19:53 +0530

    comment

--- application/views/admin/fee_discount/fee-discount.php
+++ application/views/admin/fee_discount/fee-discount.php
@@ -255,9 +255,9 @@ $currency_symbol = $this->customlib->getSchoolCurrencyFormat();
 						// console.log(item);
 						const li = document.createElement('li');
 						li.className = 'list-group-item list-group-item-action';
-						li.textContent = item.name + ' s/o '+ item.father +' ('+item.class+')';;
+						li.textContent = '(' + item.admission_no + ') '+ item.name + ' s/o '+ item.father +' ('+item.class+')';;
 						li.onclick = function () {
-							document.getElementById('searchInput').value = item.name + ' s/o '+ item.father +' ('+item.class+')';
+							document.getElementById('searchInput').value = '(' + item.admission_no + ') '+ item.name + ' s/o '+ item.father +' ('+item.class+')';
 							suggestionsList.style.display = 'none';
 							//alert(item.id);
 							$('#successMsg').hide();
