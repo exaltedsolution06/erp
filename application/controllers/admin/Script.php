a6415dd949251f6c7cdda386067f1ae7f9b6e93c
Exalted02 <exaltedsol02@gmail.com>
2025-12-26 18:37:46 +0530

    up

--- application/controllers/admin/Script.php
+++ application/controllers/admin/Script.php
@@ -489,5 +489,16 @@ class Script extends Admin_Controller
 			$this->db->query($update_sql, [$session_id]);
 		}
 		
+		//For 'certificates'.
+		$session_exists = "SHOW COLUMNS FROM `certificates` LIKE 'session_id'";
+		$session_exists_sql = $this->db->query($session_exists);		
+		if ($session_exists_sql->num_rows() == 0) {
+			$add_sql = "ALTER TABLE `certificates` ADD `session_id` INT(11) NULL DEFAULT NULL AFTER `id`";
+			$this->db->query($add_sql);
+			
+			$update_sql = "UPDATE `certificates` SET `session_id` = ?";
+			$this->db->query($update_sql, [$session_id]);
+		}
+		
     }
 }
