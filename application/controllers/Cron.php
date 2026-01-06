46981caed5f890ce734028bf37ea5ec7cf15c995
exaltedsol04 <exaltedsol04@gmail.com>
2026-01-06 20:02:20 +0530

    comment

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -19,25 +19,13 @@ class Cron extends CI_Controller
         $setting_result = $this->setting_model->getSetting();
         $this->cron_key = $setting_result->cron_secret_key;
         $this->load->model('feereminder_model');
+        $this->load->model('classsection_model');
+        $this->load->model('section_model');
+		$this->current_session = $this->setting_model->getCurrentSession();
     }
 	
-	public function changeSessions()
-    {
-        //$this->load->model('Student_model');
-
-        //$this->Student_model->update_status();
-		 $key = $this->input->get('key');
-
-        /*if ($key !== $this->cron_key) {
-            exit('Invalid Key or Direct access is not allowedwwww');
-        }*/
-
-        echo 'hello';
-        die;
-        
-    }
-
-    public function index($key = '')
+	
+	public function index($key = '')
     {
         if ($key != "" && $this->cron_key == $key) {
 
@@ -168,6 +156,170 @@ class Cron extends CI_Controller
                 }
             }
         }
+    }
+	
+	public function changeSessions($key = "")
+	{
+		
+		$key = $this->input->get('key');
+
+        if ($key !== $this->cron_key) {
+            exit('Invalid Key or Direct access is not allowe');
+        }
+		
+		
+		$this->db->select('move_students.batch_id');
+		$this->db->from('move_students');
+		$this->db->join(
+			'move_students_category',
+			'move_students.batch_id = move_students_category.batch_id'
+		);
+		$this->db->where('move_students.status', 1);
+		$this->db->where('move_students_category.status', 1);
+		$this->db->where('move_students.current_session_id', $this->current_session);
+		$this->db->group_by('batch_id');
+		$query = $this->db->get();
+		
+		foreach($query->result_array() as $result){
+			
+			$this->db->from('move_students');
+			$this->db->join('classes', 'classes.id = move_students.next_class_id');
+			$this->db->where('move_students.batch_id', $result['batch_id']);
+			$this->db->where('move_students.status', 1);
+			$qr = $this->db->where('move_students.current_session_id', $this->current_session)->get();
+			$classData = $qr->result_array();
+			//echo "<pre>";print_r($classData);
+			$sectionArr = [];
+			foreach($qr->result_array() as $val){
+				echo $val['batch_id'].'</br>';
+				//$sectionArr = [];
+				$this->db->from('move_students');
+				$this->db->join('classes', 'classes.id = move_students.next_class_id');
+				//$this->db->where('move_students.batch_id', $val['batch_id']);
+				$this->db->where('classes.id', $val['next_class_id']);
+				$this->db->where('move_students.status', 1);
+				$qr = $this->db->where('move_students.current_session_id', $this->current_session)->get();
+				$classes = $qr->row_array();
+				echo "<pre>";print_r($classes);
+				$this->db->from('classes');
+				$this->db->where('session_id',  $classes['next_session_id']);
+				$this->db->where('class', $classes['class']);
+				$query = $this->db->get();
+				if($query->num_rows() == 0)
+				{
+					$this->db->select('sections.section')->from('class_sections');
+					$this->db->join('sections', 'sections.id = class_sections.section_id');
+					$this->db->where('class_sections.class_id', $classes['next_class_id']);
+					$qr = $this->db->get();
+					$sectionData = $qr->result_array();
+					foreach($sectionData as $sec)
+					{
+						if($sec['section'] != $section_name)
+						{
+							$data = array(
+								'section' => $sec['section'],
+								'session_id' => $classes['next_session_id'],
+							);
+							$section_id = $this->section_model->add($data);
+							$sectionArr[] = $section_id;
+							$section_name = $sec['section'];
+						}
+					}
+					//echo "<pre>";print_r($sectionData);die;
+					//echo $classData['class'].'->'.$sectionData['section']."</br>";
+					//echo "<pre>";print_r($sectionArr);
+					
+					$class_array = array(
+						'class' => $classes['class'],
+						'session_id' => $classes['next_session_id'],
+					);
+					$sections = $sectionArr;
+					$this->classsection_model->add($class_array, $sections);
+				}
+				else{
+					echo 'yes';
+				}
+				
+				//echo $classes['next_class_id'].'->';
+			}
+		}
+	}
+	
+	public function changeSessions_bck($key = "")
+    {
+        //$this->load->model('Student_model');
+
+        //$this->Student_model->update_status();
+		$sectionArr = [];
+		$key = $this->input->get('key');
+
+        if ($key !== $this->cron_key) {
+            exit('Invalid Key or Direct access is not allowe');
+        }
+		//echo $this->current_session; die;
+		$this->db->select('move_students.batch_id');
+		$this->db->from('move_students');
+		$this->db->join(
+			'move_students_category',
+			'move_students.batch_id = move_students_category.batch_id'
+		);
+		$this->db->where('move_students.status', 1);
+		$this->db->where('move_students_category.status', 1);
+		$this->db->where('move_students.current_session_id', $this->current_session);
+		$this->db->group_by('batch_id');
+		$query = $this->db->get();
+		//echo "<pre>";print_r($query->result_array());die;
+		
+		foreach($query->result_array() as $result){
+			echo $result['batch_id'].'</br>';
+			$this->db->from('move_students');
+			$this->db->join('classes', 'classes.id = move_students.next_class_id');
+			$this->db->where('move_students.batch_id', $result['batch_id']);
+			$this->db->where('move_students.status', 1);
+			$qr = $this->db->where('move_students.current_session_id', $this->current_session)->get();
+			$classData = $qr->row_array();
+			
+			echo "<pre>";print_r($classData);
+			echo $classData['next_class_id'].'->';
+			
+			// check class exists in current session
+			$this->db->from('classes');
+			$this->db->where('session_id',  $classData['next_session_id']);
+			$this->db->where('class', $classData['class']);
+			$query = $this->db->get();
+			if($query->num_rows() == 0)
+			{
+					$this->db->select('sections.section')->from('class_sections');
+					$this->db->join('sections', 'sections.id = class_sections.section_id');
+					$this->db->where('class_sections.class_id', 67);
+					$qr = $this->db->get();
+					$sectionData = $qr->result_array();
+					foreach($sectionData as $sec)
+					{
+						$data = array(
+							'section' => $sec['section'],
+							'session_id' => $classData['next_session_id'],
+						);
+						//$section_id = $this->section_model->add($data);
+						$sectionArr[] = $section_id;
+					}
+					//echo "<pre>";print_r($sectionData);die;
+					echo $classData['class'].'->'.$sectionData['section']."</br>";
+					
+					$class_array = array(
+						'class' => $classData['class'],
+						'session_id' => $classData['next_session_id'],
+					);
+					$sections = $sectionArr;
+					//$this->classsection_model->add($class_array, $sections);
+				
+			}
+			else{
+				echo 'yes'; 
+			}
+		}
+        echo "<pre>";print_r($sectionArr);die;
+        
     }
 	
 
